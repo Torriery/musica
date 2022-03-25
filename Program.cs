8fe922ec166d9d51f7e8c3466722fa335a01d267
torriery <andreastorrieri3@gmail.com>
2022-03-25 19:57:47 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("A história mais velha do mundo");
+Console.ResetColor();
+
+Console.WriteLine(@"O que a gente quer
+É gostar de alguém
+E quer que esse alguém
+Goste da gente também
+
+O que a gente quer
+É gostar de alguém
+E quer que esse alguém
+Goste da gente também
+
+É a história mais velha do mundo
+Um destino que foi desenhado
+O que mais alguém pode querer
+Além de amar e ser amado?");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine(@"(O que a gente quer
+É gostar de alguém
+E quer que esse alguém
+Goste da gente também");
+Console.ResetColor();
